6d55fb38f936228bf4ce9120b90456d563ae2612
Lucas Manuel <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-11-19 06:01:41 +0200

    Criei um teste para o componente signupForm e o formulário

--- tests/Signup.test.js
+++ tests/Signup.test.js
@@ -2,7 +2,8 @@ import {describe, it, expect} from 'vitest';
 import {render, screen} from '@testing-library/react';
 
 describe('Retornar o componente signup', ()=> {
-  it('Deve retornar o formulario', ()=> {
-
+  it('Deve retornar o formulário', ()=> {
+    render(<SignupForm />)
+    expect(screen.getByRole('form')).toBeInTheDocument()
   })
 })
